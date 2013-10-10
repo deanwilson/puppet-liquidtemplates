c40992f258717a66207f45eb35c2cb216ed4e6b5
Dean Wilson <dean.wilson@gmail.com>
2013-10-10 12:18:01 +0100

    Initial release of puppet-liquidtemplates

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-puppet-liquidtemplates
-======================
+# Liquid Templates in Puppet Function #
 
-Puppet function for processing Liquid Templates
+Liquid is a 'Ruby library for rendering safe templates which cannot
+affect the security of the server they are rendered on.' This function
+allows you to write your templates using Liquid -
+
+    $ cat << 'EOC' > liquidtest/templates/test.liquid
+    SELinux is {{ selinux_config_mode | upcase }} on {{ fqdn }}
+    EOC
+
+And call them from your puppet modules -
+
+    cat << 'EOC' > liquidtest/manifests/init.pp
+    class liquidtest {
+
+      file { '/tmp/liquidtest':
+        ensure  => 'file',
+        content => liquid_template('liquidtest/test.liquid'),
+      }
+
+    }
+
+using a templating engine that does not allow arbitrary ruby code to run.
+
+More details can be found in the
+[Liquid Templates in Puppet](http://www.unixdaemon.net/tools/puppet/liquid-templates-in-puppet-initial-release.html) blog post.
+
+Note:
+This function requires the liquid gem to be available on the puppetmaster. Which you are managing via puppet, right?
---
+++ lib/puppet/parser/functions/liquid_template.rb
@@ -0,0 +1,33 @@
+module Puppet::Parser::Functions
+  require 'liquid'
+
+  newfunction(:liquid_template, :type => :rvalue, :doc => <<-EOD
+    A Puppet function that allows you to use Liquid templates instead of plain ERB.
+
+    Liquid templates are more restrictive in what they allow and do not permit execution of arbitary ruby code. So no calls to 'rm' on the puppetmaster.
+
+    Does not support inline templates. On purpose.
+  EOD
+  ) do |files|
+
+    files.collect do |file|
+      template_file = nil
+
+      Puppet.debug "Processing #{file} with liquid_template"
+
+      template_file = Puppet::Parser::Files.find_template(file, self.compiler.environment.to_s)
+      raise Puppet::ParseError, "Could not find template '#{file}'" unless template_file
+
+      # expose our scope to the templates content.
+      exposed_vars = {}
+      self.to_hash.each do |name, value|
+        realname = name.gsub(/[^\w]/, "_")
+        exposed_vars[realname] = value
+      end
+
+      template_content = ::Liquid::Template.parse(File.new(template_file).read)
+      template_content.render(exposed_vars)
+
+    end.join("")
+  end
+end
