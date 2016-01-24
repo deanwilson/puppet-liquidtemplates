3c0b64cc3c21b90ce332305b342838909a5836e6
Dean Wilson <dean.wilson@gmail.com>
2016-01-24 11:22:44 +0000

    Rubocop tidy of spec file

--- spec/functions/liquid_template_spec.rb
+++ spec/functions/liquid_template_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 describe 'liquid_template' do
-
   ############### Test normal usage
 
   describe 'when a single template is specified' do
     it 'should return the contents of the given template' do
-      should run.with_params( 'single_expansion/hello.liquid' ).and_return( /^Hello WORLD/ )
+      should run.with_params('single_expansion/hello.liquid').and_return(/^Hello WORLD/)
     end
   end
 
@@ -14,8 +13,7 @@ describe 'liquid_template' do
 
   describe 'handling expected error conditions' do
     it 'should throw Puppet::ParseError if called with no templates' do
-      should run.with_params( ).and_raise_error(Puppet::ParseError)
+      should run.with_params.and_raise_error(Puppet::ParseError)
     end
   end
-
 end
